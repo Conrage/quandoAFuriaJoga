4d43af4a587a325bf7c5277b147a11b5349a6662
João Conrad <joao.conrad@opens.com.br>
2023-07-04 15:00:00 -0300

    style: format banner and text

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
-export default function Banner({id}) {
-  const banner = useRef()
+export default function Banner({ id }) {
+  const ads = useRef()
 
   const atOptions = {
     'key': '3acdde6796f141e49d2ee80049410461',
@@ -10,22 +10,17 @@ export default function Banner({id}) {
     'params': {}
   };
   useEffect(() => {
-    if (banner.current && !banner.current?.firstChild) {
+    if (ads.current && !ads.current?.firstChild) {
       const conf = document.createElement('script')
       const script = document.createElement('script')
       script.type = 'text/javascript'
       script.src = `https://www.profitabledisplaynetwork.com/3acdde6796f141e49d2ee80049410461/invoke.js`
       conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
 
-      banner.current.append(conf);
-      banner.current.append(script);
-      script.onload = () => {
-        setTimeout(() => {
-          if (document.getElementsByTagName('iframe')[0]) document.getElementsByTagName('iframe')[0].className = "change-ad-size";
-        }, 200)
-      }
+      ads.current.append(conf);
+      ads.current.append(script);
     }
-  }, [banner])
+  }, [ads])
 
-  return <div className="w-0 sm:w-1/5 h-full flex flex-col gap-4 border border-gray-200 py-10 items-center text-white text-center"><h3 className="font-montserrat w-fit font-extrabold uppercase tracking-[-0.08rem] text-sm text-mine-shaft-900">Esmola do Crazynn</h3><div className="rounded-md border-2 w-[160px] h-[600px] bg-black" ref={banner}></div> </div>
+  return <div className="w-0 md:w-1/5 h-full flex flex-col gap-4 border border-gray-200 py-10 items-center text-white text-center"><h3 className="font-montserrat w-fit font-extrabold uppercase tracking-[-0.08rem] text-sm text-mine-shaft-900">Esmola do Crazynn</h3><div className="rounded-md border-2 w-[160px] h-[600px] bg-black" ref={ads}></div> </div>
 }
--- app/components/Banner2.js
+++ app/components/Banner2.js
@@ -1,31 +1,26 @@
 import { useEffect, useRef } from 'react'
 export default function Banner2({ id }) {
-    const bannerRef = useRef()
+  const bannerRef = useRef()
 
-    const atOptions = {
-        'key': '249035ff3765c4fd1c0cb951d60b3d38',
-        'format': 'iframe',
-        'height': 300,
-        'width': 160,
-        'params': {}
-    };
-    useEffect(() => {
-        if (bannerRef.current && !bannerRef.current?.firstChild) {
-            const conf = document.createElement('script')
-            const script = document.createElement('script')
-            script.type = 'text/javascript'
-            script.src = `https://www.profitabledisplaynetwork.com/249035ff3765c4fd1c0cb951d60b3d38/invoke.js`
-            conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+  const atOptions = {
+    'key': '249035ff3765c4fd1c0cb951d60b3d38',
+    'format': 'iframe',
+    'height': 300,
+    'width': 160,
+    'params': {}
+  };
+  useEffect(() => {
+    if (bannerRef.current && !bannerRef.current?.firstChild) {
+      const conf = document.createElement('script')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.profitabledisplaynetwork.com/249035ff3765c4fd1c0cb951d60b3d38/invoke.js`
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
 
-            bannerRef.current.append(conf);
-            bannerRef.current.append(script);
-            script.onload = () => {
-                setTimeout(() => {
-                    if (document.getElementsByTagName('iframe')[0]) document.getElementsByTagName('iframe')[0].className = "change-ad-size";
-                }, 200)
-            }
-        }
-    }, [bannerRef])
+      bannerRef.current.append(conf);
+      bannerRef.current.append(script);
+    }
+  }, [bannerRef])
 
-    return <div className="w-0 sm:w-1/5 h-full flex flex-col gap-4 border border-gray-200 py-10 items-center text-white text-center"><h3 className="font-montserrat w-fit font-extrabold uppercase tracking-[-0.08rem] text-sm text-mine-shaft-900">Esmola do Crazynn</h3><div className="rounded-md border-2 w-[160px] h-[300px] bg-black" ref={bannerRef}></div> </div>
+  return <div className="w-0 md:w-1/5 h-full flex flex-col gap-4 border border-gray-200 py-10 items-center text-white text-center"><h3 className="font-montserrat w-fit font-extrabold uppercase tracking-[-0.08rem] text-sm text-mine-shaft-900">Esmola do Crazynn</h3><div className="rounded-md border-2 w-[160px] h-[300px] bg-black" ref={bannerRef}></div> </div>
 }
