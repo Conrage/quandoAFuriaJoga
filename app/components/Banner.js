8070d8be3c669819def3dd9cdc72c5d83d2ae80f
João Conrad <joao.conrad@opens.com.br>
2023-07-04 14:11:05 -0300

    style: format banner

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -3,12 +3,12 @@ export default function Banner() {
     const banner = useRef()
 
     const atOptions = {
-		'key' : '3acdde6796f141e49d2ee80049410461',
-		'format' : 'iframe',
-		'height' : 600,
-		'width' : 160,
-		'params' : {}
-	};
+        'key': '3acdde6796f141e49d2ee80049410461',
+        'format': 'iframe',
+        'height': 600,
+        'width': 160,
+        'params': {}
+    };
     useEffect(() => {
         if (banner.current && !banner.current?.firstChild) {
             const conf = document.createElement('script')
@@ -20,6 +20,8 @@ export default function Banner() {
             banner.current.append(conf)
             banner.current.append(script)
         }
+
+        document.getElementsByTagName('iframe')[0].className = "change-ad-size";
     }, [banner])
 
     return <div className="w-0 sm:w-1/5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
