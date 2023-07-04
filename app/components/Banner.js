87228d46fa66598eebef30c8de3b368758fcc520
João Conrad <joao.conrad@opens.com.br>
2023-07-04 14:18:55 -0300

    style: format banner

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,28 +1,31 @@
 import { useEffect, useRef } from 'react'
 export default function Banner() {
-    const banner = useRef()
+  const banner = useRef()
 
-    const atOptions = {
-        'key': '3acdde6796f141e49d2ee80049410461',
-        'format': 'iframe',
-        'height': 600,
-        'width': 160,
-        'params': {}
-    };
-    useEffect(() => {
-        if (banner.current && !banner.current?.firstChild) {
-            const conf = document.createElement('script')
-            const script = document.createElement('script')
-            script.type = 'text/javascript'
-            script.src = `https://www.profitabledisplaynetwork.com/3acdde6796f141e49d2ee80049410461/invoke.js`
-            conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+  const atOptions = {
+    'key': '3acdde6796f141e49d2ee80049410461',
+    'format': 'iframe',
+    'height': 600,
+    'width': 160,
+    'params': {}
+  };
+  useEffect(() => {
+    if (banner.current && !banner.current?.firstChild) {
+      const conf = document.createElement('script')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.profitabledisplaynetwork.com/3acdde6796f141e49d2ee80049410461/invoke.js`
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
 
-            banner.current.append(conf)
-            banner.current.append(script)
-        }
+      banner.current.append(conf);
+      banner.current.append(script);
+      script.onload = () => {
+        setTimeout(() => {
+          if (document.getElementsByTagName('iframe')[0]) document.getElementsByTagName('iframe')[0].className = "change-ad-size";
+        }, 200)
+      }
+    }
+  }, [banner])
 
-        document.getElementsByTagName('iframe')[0].className = "change-ad-size";
-    }, [banner])
-
-    return <div className="w-0 sm:w-1/5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
+  return <div className="w-0 sm:w-1/5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
 }
