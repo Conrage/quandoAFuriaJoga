b3da187f510df5bd8d1e11064402603883fa65b1
João Conrad <joao.conrad@opens.com.br>
2023-07-05 11:16:28 -0300

    style: remove title esmola

--- app/components/Banner2.js
+++ app/components/Banner2.js
@@ -24,5 +24,5 @@ export default function Banner2({ id }) {
     }, 500)
   }, [bannerRef])
 
-  return <div className="w-0 md:w-1/5 h-full flex flex-col gap-4 border border-gray-200 py-10 items-center text-white text-center"><h3 className="font-montserrat w-fit font-extrabold uppercase tracking-[-0.08rem] text-sm text-mine-shaft-900">Esmola do Crazynn</h3><div className="rounded-md overflow-clip border-2 w-[160px] h-[600px]" ref={bannerRef}></div> </div>
+  return <div className="w-0 md:w-1/5 h-full flex flex-col gap-4 border border-gray-200 py-10 items-center text-white text-center"><div className="rounded-md overflow-clip border-2 w-[160px] h-[600px]" ref={bannerRef}></div> </div>
 }
